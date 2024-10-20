2e559bf29e284189c7f6acfc9b922b15b564e5b6
amritchapai <chapaiamrit@gmail.com>
2024-10-20 21:48:12 +0545

    complete like and unlike

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -1,4 +1,3 @@
-
 import sharp from "sharp";
 import cloudinary from "../utils/cloudinary.js";
 import Post from "../models/Posts.js";
@@ -41,21 +40,93 @@ export const addPost = async (req, res) => {
       });
     }
     user.posts.push(post._id);
-    await user.save(); 
+    await user.save();
     //populate in owner
-    await post.populate({path:'owner', select:'-password'})
+    await post.populate({ path: "owner", select: "-password" });
 
     return res.status(201).json({
-        message: "new post added",
-        post,
-        success: true
-    })
+      message: "new post added",
+      post,
+      success: true,
+    });
   } catch (error) {
     console.log(error);
   }
 };
 
 //to edit posts
-export const editPost = async (req, res)=>{
-  
-}
+export const editPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    if (!postId) {
+      return res.status(400).json({
+        message: "post does not exist",
+        success: false,
+      });
+    }
+    //get the caption updated
+    const updatedCaption = req.body.caption;
+    //update the caption
+    const updatedPost = await Post.findByIdAndUpdate(postId, {
+      caption: updatedCaption,
+    });
+    return res.status(200).json({
+      message: "post edited",
+      success: true,
+      updatedPost,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const likeUnlikePost = async (req, res) => {
+  try {
+    const userId = req.id;
+    const postId = req.params.id;
+
+    //find the post
+    const postToLike = await Post.findById(postId);
+    if (!postToLike) {
+      return res.status(400).json({
+        message: " post does not exist",
+        success: false,
+      });
+    }
+    //check whether the user has liked or not
+    const hasLiked = postToLike.likes.includes(userId);
+    if (!userLikes) {
+      const post = await Post.findByIdAndUpdate(
+        postId,
+        {
+          $push: {
+            likes: userId,
+          },
+        },
+        { new: true }
+      );
+      return res.status(200).json({
+        message: "Post liked",
+        success: true,
+        post,
+      });
+    }
+    //to unlike pull the id from likes in post
+    const post = await Post.findByIdAndUpdate(
+      postId,
+      {
+        $pull: {
+          likes: userId,
+        },
+      },
+      { new: true }
+    );
+    return res.status(200).json({
+      message: "Post unliked",
+      success: true,
+      post,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};

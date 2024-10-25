4befa739bae5a08159f6a0cfa816fd291a70e8a7
amritchapai <chapaiamrit@gmail.com>
2024-10-25 23:05:28 +0545

    completed postRoutes

--- backend/routers/postRoutes.js
+++ backend/routers/postRoutes.js
@@ -1,10 +1,13 @@
 import express from "express"
-import { addPost, deletePost, editPost, likeUnlikePost } from "../controller/postController";
+import { addPost, deletePost, editPost, getAllPosts, getUserPost, likeUnlikePost } from "../controller/postController";
 import authentication from "../middleware/authentication";
+import upload from "../middleware/multer";
 
 const router = express.Router();
 
-router.post("/addPost",authentication, addPost);
+router.post("/addPost",authentication, upload.single('image'), addPost);
 router.post("/editPost/:id", authentication, editPost);
 router.post("/likeUnlike/:id", authentication, likeUnlikePost);
 router.delete("/deletePost/:id", authentication, deletePost);
+router.get("/getAllPost", authentication, getAllPosts);
+router.get("/getUserPosts", authentication, getUserPost);

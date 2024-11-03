5e752b1a23826c2a072caf6a2aad2553ccee606c
amritchapai <chapaiamrit@gmail.com>
2024-11-03 23:59:41 +0545

    started in create post

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from './ui/dialog'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { Textarea } from './ui/textarea'
+import { useRef } from 'react'
+import { Button } from './ui/button'
+import { DataURL } from '@/lib/utils'
 
 const CreatePost = ({open, setOpen}) => {
+    const [caption, setCaption] = useState("")
+    const [image, setImage] = useState("")
+    const [preview, setPreview] = useState("")
+    const imageRef = useRef();
+    const fileChangeHandler= async(e)=>{
+        const file = e.target.files?.[0];
+        if(file){
+            setImage(file);
+            const dataURL = await  DataURL(file);
+            setPreview(dataURL);
+        }
+    }
   return (
     <Dialog open={open}>
         <DialogContent onInteractOutside= {()=>{setOpen(false)}} className="max-w-4xl">
@@ -16,9 +32,16 @@ const CreatePost = ({open, setOpen}) => {
                     <span className='font-semibold text-sm'>Username</span>
             </div>
             <hr className='bg-red-600'/>
-            <div>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam sequi voluptatum veritatis doloremque unde deserunt iure magnam placeat ab laudantium, commodi at nihil quos beatae temporibus repudiandae ducimus voluptatibus reiciendis fugiat officia ratione maxime, eaque recusandae! Omnis necessitatibus perspiciatis earum illo quibusdam quas molestiae ducimus hic assumenda numquam fuga consectetur, libero, consequatur reiciendis? Quis in, sapiente nobis cum tempore consectetur.
-            </div>
+                <Textarea className = "outline-none border-none focus-visible:ring-0" placeholder="Add a caption..."/>
+                {
+                    preview && (
+                        <div className='w-full h-64 flex items-center justify-center'>
+                            <img src={preview} alt = "uploaded image" className="h-full w-full object-cover" />
+                        </div>
+                    )
+                }
+                <input ref={imageRef} type='file' hidden onChange={fileChangeHandler}/>
+                <Button onClick={()=>imageRef.current.click()} className="w-fit mx-auto" >Add from this device</Button>
         </DialogContent>
     </Dialog>
   )
